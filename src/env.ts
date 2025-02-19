edf687d5a710e3300be72a08f660dd30a3975f6d
larissadantier <larissa_dantier@hotmail.com>
2025-02-18 23:45:44 -0300

    feat: configure redis and drizzle with postgres

--- src/env.ts
+++ src/env.ts
@@ -2,14 +2,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
+  WEB_URL: z.string().url(),
 })
-
-const _env = envSchema.safeParse(envSchema)
-
-if (_env.success === false) {
-  console.error('Invalid environment variables!', _env.error.format())
-
-  throw new Error('Invalid environment variables.')
-}
-
-export const env = _env.data
+export const env = envSchema.parse(process.env)
